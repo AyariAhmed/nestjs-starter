889d8b7d7a234272f8dd73ca38700f4dcad3f9b7
AyariAhmed <ayari17@hotmail.fr>
2021-04-27 17:48:50 +0100

    added phone/email helper functions

--- src/auth/client.repository.ts
+++ src/auth/client.repository.ts
@@ -1,8 +1,69 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Client } from "./entities/client.entity";
-
+import * as bcrypt from "bcrypt";
+import { SignupCredentialsDto } from "./dto/signup-credentials.dto";
+import { Address } from "./entities/address.entity";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { LoginCredentialsDto } from "./dto/login-credentials.dto";
 
 @EntityRepository(Client)
-export class ClientRepository extends Repository<Client>{
+export class ClientRepository extends Repository<Client> {
+
+  async signup(signupCredentialsDto: SignupCredentialsDto): Promise<Object | void> {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      phone,
+      birthdate,
+      role,
+      governorate,
+      municipality,
+      street,
+      location
+    } = signupCredentialsDto;
+
+    const address: Address = new Address(governorate, municipality, street, location);
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user: Client = new Client(firstName, lastName, email, hashedPassword, phone, birthdate, role, address);
+    try {
+      await address.save();
+      await user.save();
+    } catch (error) {
+      if (error.code === "23505")
+        throw new ConflictException("Email/Phone number already exists!");
+      else throw new InternalServerErrorException();
+    }
+    delete user.password;
+    return user;
+  }
+
+
+  async validateUserPassword(loginCredentialsDto: LoginCredentialsDto): Promise<Object | null> {
+    const { email_or_phone, password } = loginCredentialsDto;
+
+    const user: User | undefined = await this.findOne({ username });
+
+    if (user && await bcrypt.compare(password, user.password)) {
+      delete user.password;
+      return user;
+    } else return null;
+
+  }
+
+  private isEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  private isPhone(phone) {
+    const re = /^\d{8}$/;
+    return re.test(phone);
+  }
+
 
 }
