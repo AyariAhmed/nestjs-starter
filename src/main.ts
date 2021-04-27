64d2bbf569fd4a8c885be2b23b69f2a43557cace
AyariAhmed <ayari17@hotmail.fr>
2021-04-27 11:41:34 +0100

    added required packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const defaultPort = 3000;
+  await app.listen(process.env.PORT || defaultPort,()=>{
+    logger.log(`Application listening on port : ${process.env.PORT || defaultPort}`)
+  });
 }
 bootstrap();
